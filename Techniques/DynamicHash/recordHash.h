f521f969fdbbf6aa670d8adc12f4d839ebf6e7bb
jneirar <jorge.neira@utec.edu.pe>
2021-10-16 22:10:21 -0500

    Add erase in Hash and format to code

--- Techniques/DynamicHash/recordHash.h
+++ Techniques/DynamicHash/recordHash.h
@@ -4,36 +4,42 @@
 #include "../lib.h"
 
 template <typename TKey>
-class RecordHash{
+class RecordHash
+{
     TKey key;
     char nombre[20];
+
 public:
-    void setData(TKey key){
+    void setData(TKey key)
+    {
         this->key = key;
         string nombre = "Alumno " + to_string(key);
         strcpy(this->nombre, nombre.c_str());
     }
 
-    void showData() {
+    void showData()
+    {
         cout << "\tKey: " << key << "\n";
         cout << "\tNombre: " << nombre << "\n\n";
     }
-    TKey getKey() {
+    TKey getKey()
+    {
         return key;
     }
 
-    RecordHash& operator =(const RecordHash& a){
+    RecordHash &operator=(const RecordHash &a)
+    {
         this->key = a.key;
-        for(int i = 0; i < 20; i++) this->nombre[i] = a.nombre[i];
+        for (int i = 0; i < 20; i++)
+            this->nombre[i] = a.nombre[i];
         return *this;
     }
-    bool operator <(const RecordHash& a) { return this->key < a.key; }
-    bool operator >(const RecordHash& a) { return this->key > a.key; }
-    bool operator <=(const RecordHash& a){ return this->key <= a.key;}
-    bool operator >=(const RecordHash& a){ return this->key >= a.key;}
-    bool operator ==(const RecordHash& a){ return this->key == a.key;}
-    bool operator !=(const RecordHash& a){ return this->key != a.key;}
+    bool operator<(const RecordHash &a) { return this->key < a.key; }
+    bool operator>(const RecordHash &a) { return this->key > a.key; }
+    bool operator<=(const RecordHash &a) { return this->key <= a.key; }
+    bool operator>=(const RecordHash &a) { return this->key >= a.key; }
+    bool operator==(const RecordHash &a) { return this->key == a.key; }
+    bool operator!=(const RecordHash &a) { return this->key != a.key; }
 };
 
-
-#endif  //RECORD_HASH.H
+#endif //RECORD_HASH.H
--- Techniques/Sequential/record.h
+++ Techniques/Sequential/record.h
@@ -4,13 +4,16 @@
 #include "../lib.h"
 
 template <typename TKey>
-class Record{
+class Record
+{
     TKey key;
     char nombre[20];
     long next = -1;
     char file = 'd';
+
 public:
-    void setData(TKey key){
+    void setData(TKey key)
+    {
         this->key = key;
         string nombre = "Alumno " + to_string(key);
         strcpy(this->nombre, nombre.c_str());
@@ -18,39 +21,45 @@ public:
         char file = 'd';
     }
 
-    void showData() {
+    void showData()
+    {
         cout << "Key: " << key << "\n";
         cout << "Nombre: " << nombre << "\n";
         cout << "Next : " << next << "\n";
         cout << "NextFile : " << file << "\n\n";
     }
-    void setNext(long _next, char _file){
+    void setNext(long _next, char _file)
+    {
         next = _next;
         file = _file;
     }
-    long getNext(){
+    long getNext()
+    {
         return next;
     }
-    char getFile(){
+    char getFile()
+    {
         return file;
     }
-    int getKey(){
+    int getKey()
+    {
         return key;
     }
-    Record& operator =(const Record& a){
+    Record &operator=(const Record &a)
+    {
         this->key = a.key;
-        for(int i = 0; i < 20; i++) this->nombre[i] = a.nombre[i];
+        for (int i = 0; i < 20; i++)
+            this->nombre[i] = a.nombre[i];
         this->next = a.next;
         this->file = a.file;
         return *this;
     }
-    bool operator <(const Record& a) { return this->key < a.key; }
-    bool operator >(const Record& a) { return this->key > a.key; }
-    bool operator <=(const Record& a){ return this->key <= a.key;}
-    bool operator >=(const Record& a){ return this->key >= a.key;}
-    bool operator ==(const Record& a){ return this->key == a.key;}
-    bool operator !=(const Record& a){ return this->key != a.key;}
+    bool operator<(const Record &a) { return this->key < a.key; }
+    bool operator>(const Record &a) { return this->key > a.key; }
+    bool operator<=(const Record &a) { return this->key <= a.key; }
+    bool operator>=(const Record &a) { return this->key >= a.key; }
+    bool operator==(const Record &a) { return this->key == a.key; }
+    bool operator!=(const Record &a) { return this->key != a.key; }
 };
 
-
-#endif  //RECORD.H
+#endif //RECORD.H
